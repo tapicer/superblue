03fc21ac7354fad255300c966338684513889e6e
Jonathan Tapicer <tapicer@gmail.com>
2013-06-12 20:53:10 -0300

    Changed to Twitter API v1.1.

--- get_data.php
+++ get_data.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set('America/Argentina/Buenos_Aires');
-$items = json_decode(file_get_contents('https://api.twitter.com/1/statuses/user_timeline.json?screen_name=DolarBlue&count=50'));
+$items = json_decode(shell_exec('curl --get \'https://api.twitter.com/1.1/statuses/user_timeline.json\' --data \'count=50&screen_name=dolarblue\' --header \'Authorization: OAuth oauth_consumer_key="Np5h53djFakmjcY1kJY8oA", oauth_nonce="d452d260938c1ffdcdd799e097f14e5c", oauth_signature="ptEG95m4TztEil7t0n%2Btl%2BdH54I%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1371080636", oauth_token="15245643-3CUKexYi12SJvvcWWGW2BruxDnVLmX91OosGbCrB7", oauth_version="1.0"\''));
 $data = array();
 foreach ($items as $item) {
         $timestamp = strtotime($item->created_at);
