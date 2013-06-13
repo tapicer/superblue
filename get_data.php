d298375f93b6c0a7c911383b80e9777d8b96f4d1
Jonathan Tapicer <tapicer@gmail.com>
2013-06-13 13:54:03 +0000

    Changed Twitter lib.

--- get_data.php
+++ get_data.php
@@ -1,17 +1,23 @@
 <?php
 date_default_timezone_set('America/Argentina/Buenos_Aires');
-$items = json_decode(shell_exec('curl --get \'https://api.twitter.com/1.1/statuses/user_timeline.json\' --data \'count=50&screen_name=dolarblue\' --header \'Authorization: OAuth oauth_consumer_key="Np5h53djFakmjcY1kJY8oA", oauth_nonce="d452d260938c1ffdcdd799e097f14e5c", oauth_signature="ptEG95m4TztEil7t0n%2Btl%2BdH54I%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1371080636", oauth_token="15245643-3CUKexYi12SJvvcWWGW2BruxDnVLmX91OosGbCrB7", oauth_version="1.0"\''));
+require_once ('codebird.php');
+\Codebird\Codebird::setConsumerKey('Np5h53djFakmjcY1kJY8oA', '1H2H8ErxcILCH6tehbG4xUVzGC2qdEZlG21xTMgU');
+$cb = \Codebird\Codebird::getInstance();
+$cb->setToken('15245643-3CUKexYi12SJvvcWWGW2BruxDnVLmX91OosGbCrB7', 'larXQZEcgRfQgvfqBvTSWbHzaIpbfqW0c2nRWtqc');
+$items = $cb->statuses_userTimeline('count=50&screen_name=dolarblue');
 $data = array();
 foreach ($items as $item) {
-        $timestamp = strtotime($item->created_at);
-        if (preg_match('/\$(\d+,\d+)/', $item->text, $matches)) {
-                $value = str_replace(',', '.', $matches[1]);
-                $day = date('d-m-Y', $timestamp);
-                if (!isset($data[$day])) {
-                        $data[$day] = $value;
-                }
-                //echo date('d-m-Y', $timestamp) . ' ' . $value . '<br/>';
-        }
+	if (is_object($item)) {
+		$timestamp = strtotime($item->created_at);
+		if (preg_match('/\$(\d+,\d+)/', $item->text, $matches)) {
+			$value = str_replace(',', '.', $matches[1]);
+			$day = date('d-m-Y', $timestamp);
+			if (!isset($data[$day])) {
+				$data[$day] = $value;
+			}
+			//echo date('d-m-Y', $timestamp) . ' ' . $value . '<br/>';
+		}
+	}
 }
 //echo '<pre>';
 //var_dump($data);
